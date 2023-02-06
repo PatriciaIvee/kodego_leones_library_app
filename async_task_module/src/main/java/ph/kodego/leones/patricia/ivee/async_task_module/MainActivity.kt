2751138266bf10b6566e7abc4fc8f5f7f6c84e97
PatriciaIvee <patriciaiveeleones@gmail.com>
2023-02-06 20:16:47 +0800

    Update Submission for Activity 11 and 12 partial answer Can't display recycler with authors need to reCheck
    Added Module Async task for Activity Async task

---
+++ async_task_module/src/main/java/ph/kodego/leones/patricia/ivee/async_task_module/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.kodego.leones.patricia.ivee.async_task_module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
